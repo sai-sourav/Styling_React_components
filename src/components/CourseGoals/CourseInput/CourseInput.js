32c079cbb88c644283879303604588fe33b30bfb
sai-sourav <saisourav123@gmail.com>
2023-02-05 10:13:53 +0530

    dynamic classes

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setisValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setisValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setisValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isValid ? '':'invalid'}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button className={`button ${isValid ? '':'invalid'}`} type="submit">Add Goal</Button>
     </form>
   );
 };
